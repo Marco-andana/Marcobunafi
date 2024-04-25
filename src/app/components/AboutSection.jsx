66e44314bd48b20cb8f3ef0ede9742abd5d2e43f
Marco-andana <marcoasb56@gmail.com>
2024-04-25 11:04:02 +0700

    a

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,104 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>MySQL</li>
+        <li>PHP</li>
+        <li>Python</li>
+        <li>Laravel</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Bina Nusantara University, Indonesia</li>
+        <li>Freecodecamp</li>
+        <li>Dicoding Academy</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>AWS Cloud Practitioner</li>
+        <li>JavaScript</li>
+        <li>Web Developer</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/aboutme.jpg" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-6xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+          I am a passionate full-stack developer with a diverse skill 
+          set encompassing software engineering, web development, and 
+          ML/AI engineering. With a keen interest in robotics, 
+          I constantly seek new challenges and opportunities to apply 
+          my knowledge and expertise in these fields. Whether 
+          it's crafting efficient algorithms, building robust web applications, 
+          or exploring the possibilities of artificial intelligence, 
+          I am dedicated to pushing the boundaries of technology 
+          and creating innovative solutions.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
